1ab67b2c9e7fda8839b1c8612abd7e57151c9dbb
Eng.Wesal <eng-wesal@eng.wesal-HP-250-G7-Notebook-PC>
2021-04-22 00:10:56 +0300

    number comparison

---
+++ larg-number.js
@@ -0,0 +1,50 @@
+
+  {
+    const val1 = Number(prompt('Enter first number')) || 0;
+    const val2 = Number(prompt('Enter second number')) || 0;
+  
+    
+    
+
+   
+    if( Number(val1) >  Number(val2))
+      {
+          alert ( "The larger number is" + "  " + Number(val1));
+      }
+      if( Number(val1) < Number(val2))
+      {
+          
+          alert ( "The larger number is" + "  " + Number(val2));
+      }
+      else{
+        alert( "both values are equal");
+      }
+      
+    
+}
+
+      
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+         
+    
+ 
+   
+ 
+ 
+    
